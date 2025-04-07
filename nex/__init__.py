2051b2b533f1f21851ae696eae637ff8e9c77018
yousuf shahzad <yousufshahzadd@yahoo.com>
2025-04-07 09:22:47 +0100

    basic init structure

---
+++ nex/__init__.py
@@ -0,0 +1,9 @@
+from .server_manager import ServerManager
+from .cli import cli
+
+"""
+Minecraft CLI Server Tool - A command-line tool for managing Minecraft servers.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Yousuf Shahzad"
